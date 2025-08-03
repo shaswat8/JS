37d78d50e0cc6a5be2341cf6f2a67ae3265c81fd
shaswat8 <136618728+shaswat8@users.noreply.github.com>
2025-08-03 10:53:19 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use srtict"; // treat all JS code as newer version
+
+// alert("3 + 3") // weeb are using nodejs,not broswer
+
+console.log(3 + 3);
+
+console.log("Shaswat")
+
+
+let name = "shaswat"
+let age = 20
+let loogedIn = false
+let state;
+
+// number => to power 53
+// bigint 
+// srting => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof "shaswat");
+console.log(typeof nul); // on=bject
+console.log(typeof undefined); // undefined
+
+
