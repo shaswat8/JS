91ff781539d766a1582c15b715d9b0ee61f7acbd
shaswat8 <136618728+shaswat8@users.noreply.github.com>
2025-08-04 11:32:27 +0000

    strings in JS

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "shaswat"
+const repoCount = 50
+
+// console.log(name + repoCount +"value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("shaswat_sh")
+
+// console.log(gameName[0]);
+// console.log(gameNmae.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("t"));
+
+
+//  const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-4,8);
+// console.log(anotherString);
+
+const newStringOne = "  shaswat  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// const url = " https://www.youtube.com/@shaswat%20yadav "
+// console.log(url.replace("%20" , "-"))
+
+// console.log(url.includes("shaswat"))
+
+console.log(gameName.split("_"));
